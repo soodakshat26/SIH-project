eeb9a947d5b23bf18638f4057ecf1a9fdf329f95
Chahat <144444393+chahatbashani2304@users.noreply.github.com>
2024-09-04 12:40:39 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <a href="/">Home</a>
-      <a href="/features">Features</a>
-      <a href="/about">About</a>
-      <a href="/testimonials">Testimonials</a>
+      <li><a href="#features">Features</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#testimonials">Testimonials</a></li>
     </div>
   );
 };
